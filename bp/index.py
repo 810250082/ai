aec8ce27459f408d13155366a188c9839721fba1
sqy <1245@qq.com>
2019-04-22 22:59:54 +0800

    bf补充

--- bp/index.py
+++ bp/index.py
@@ -2,7 +2,7 @@
 """
 BP算法
 """
-import numpy as np
+from numpy import *
 
 
 """
@@ -11,32 +11,37 @@ import numpy as np
 
 
 def load_data():
-    input = [2, 3]
-    out = [0.1, 0.9]
-    return input, out
+    inputs = mat([2, 3])
+    out = mat([0.1, 0.9])
+    b = array([1, 1])
+    return inputs, out, b
 
 
 def sigmod(x):
-    return 1.0 / (1 + np.exp(-1 * x))
+    return 1.0 / (1 + exp(-1 * x))
 
 
-def bf(input, output, step=0.001, max_iter=1000):
-    input.append(1)
-    input = np.array(input)
-    output = np.array(output)
-    m_input = input.shape[1]
-    m_out = output.shape[0]
-    w1 = np.random.random((m_input, 3))
-    w2 = np.random.random((4, m_out))
+def bf(inputs, output, b, step=0.001, max_iter=1000):
+    m_input = inputs.shape[1]
+    m_out = output.shape[1]
+    w1 = random.random((m_input, 3))
+    w2 = random.random((3, m_out))
 
     for i in range(max_iter):
-        h_net = np.dot(input, w1)
+        h_net = inputs * w1 + b[0]
         h_out = sigmod(h_net)
-        h_out = np.vstack((h_out, [1]))
-        o_net = np.dot(h_out, w2)
+        o_net = h_out * w2 + b[1]
         o_out = sigmod(o_net)
         offset = o_out - output
-        error = 1.0 / 2 * np.dot(offset, offset)
-        g = (output - o_out) * o_out * (1 - o_out)
-        w2 = np.dot(h_out, g)
-        # w1 = g1 *
+        error = 1.0 / 2 * offset * offset.transpose()
+        g = -(output.A - o_out.A) * o_out.A * (1 - o_out.A)
+        w2 = w2 - step * h_out.transpose() * g
+        g1 = g * w2.transpose()
+        w1 = w1 - step * inputs.transpose() * g1
+        b[1] = b[1] - step * sum(g)
+        b[0] = b[0] - step * sum(g1)
+        print('error: {}'.format(error))
+
+
+inputs, out, b = load_data()
+bf(inputs, out, b, step=0.001, max_iter=500000)
