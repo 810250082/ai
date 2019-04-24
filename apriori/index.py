a4de816e34ab6ad374fe2eaca6c3f34820039bb9
sqy <1245@qq.com>
2019-04-24 09:08:50 +0800

    apriori 添加关联规则

--- apriori/index.py
+++ apriori/index.py
@@ -2,6 +2,16 @@
 
 
 from numpy import *
+from itertools import combinations
+
+
+# a = [1, 2, 3, 4]
+# b = [frozenset([i]) for i in a]
+# b = frozenset([1, 2, 3])
+# c = combinations(b, 2)
+# e = 1
+# for item in combinations(b, 2):
+#     print(item)
 
 
 def load_data():
@@ -25,11 +35,14 @@ def get_L1(data, min_support=0.5):
                 counter[item] += 1
     # 筛选频繁项集
     m = float(len(data))
-    s = set([])
+    s = []
+    support_dict = {}
     for key, val in counter.items():
-        if val / m >= min_support:
-            s.add((key,))
-    return s
+        support = val / m
+        if support >= min_support:
+            s.append(frozenset([key]))
+        support_dict[frozenset([key])] = support
+    return s, support_dict
 
 
 def next_L(pre_set):
@@ -40,8 +53,7 @@ def next_L(pre_set):
     :return:
     """
     pre_num = len(pre_set)
-    pre_set = list(pre_set)
-    pre_item_num = len(list(pre_set)[0])
+    pre_item_num = len(pre_set[0])
     if len(pre_set) == 1:
         return []
     # 组合
@@ -50,9 +62,9 @@ def next_L(pre_set):
     box = set([])
     while i < pre_num - 1:
         while j < pre_num:
-            s = set(pre_set[i]+pre_set[j])
+            s = pre_set[i].union(pre_set[j])
             if len(s) == pre_item_num+1:
-                box.add(tuple(s))
+                box.add(s)
             j += 1
         i += 1
         j = i + 1
@@ -64,37 +76,94 @@ def filter_item(data, item_s, min_support=0.5):
     counter = {}
     for record in data:
         for item in item_s:
-            if set(item).issubset(set(record)):
+            if item.issubset(set(record)):
                 if item not in counter:
                     counter[item] = 1
                 else:
                     counter[item] += 1
     # 筛选频繁项集
     m = float(len(data))
-    s = set([])
+    s = []
+    support_dict = {}
     for key, val in counter.items():
-        if val / m >= min_support:
-            s.add(key)
-    return s
+        support = val / m
+        if support >= min_support:
+            s.append(key)
+        support_dict[key] = support
+    return s, support_dict
 
 
 def apriori(data, min_support=0.5):
-    s1 = get_L1(data, min_support)
-    if not s1:
+    s, support_dict = get_L1(data, min_support)
+    if not s:
         return []
     k = 2
-    box = s1
-    while k <= len(s1):
-        next_s = next_L(s1)
-        filter_s = filter_item(data, next_s, min_support)
-        if not filter_s:
+    count = len(s)
+    box = s[:]
+    while k <= count:
+        next_s = next_L(s)
+        s, supports = filter_item(data, next_s, min_support)
+        support_dict.update(supports)
+        if not s:
             break
-        box |= filter_s
+        box.extend(s)
         k += 1
+    return box, support_dict
+
+
+def gen_rule(data, support_dict, min_support=0.7):
+    box = []
+    for item in data:
+        if len(item) == 1:
+            continue
+        neces = [[]]
+        for k in range(1, len(item)):
+            # 获取长度为k的所有排列组合
+            coms = list(combinations(item, k))
+            # 筛选以 nece_item 为子集的所有项
+            coms_copy = coms[:]
+            for com in coms:
+                b = 0
+                for nece in neces:
+                    if set(nece).issubset(set(com)):
+                        b = 1
+                        break
+                if b == 0:
+                    coms_copy.remove(com)
+            # 生成规则
+            all_rule = []
+            for com in coms_copy:
+                arr = [1, 1, 1]
+                arr[1] = frozenset(com)
+                arr[0] = item.difference(arr[1])
+                arr[2] = support_dict[item] * 1.0 / support_dict[arr[0]]
+                all_rule.append(arr)
+            # 筛选规则
+            neces = []
+            for rule in all_rule:
+                # 判断可信度
+                if rule[2] >= min_support:
+                    box.append(rule)
+                    neces.append(rule[1])
+            k += 1
     return box
 
 
-data = load_data()
-a = apriori(data, min_support=0.7)
-b = 1
+# data = load_data()
+# box, support_dict = apriori(data, min_support=0.5)
+# rules = gen_rule(box, support_dict, min_support=0.5)
+# b = 1
+
+if __name__ == '__main__':
+    data = []
+    with open('mushroom.dat', 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            data.append(line.strip().split(' '))
+    # data = datas[datas[0] == 2][:, 1:].tolist()
+    datas = array(data)
+    data = datas[datas[:, 0] == '2'][:, 1:].tolist()
+    box, support_dict = apriori(data, min_support=0.8)
+    rules = gen_rule(box, support_dict, min_support=0.8)
+    print(rules)
 
--- machinelearninginaction/Ch11/apriori.py
+++ machinelearninginaction/Ch11/apriori.py
@@ -61,7 +61,7 @@ def apriori(dataSet, minSupport = 0.5):
         k += 1
     return L, supportData
 
-def generateRules(L, supportData, minConf=0.7):  #supportData is a dict coming from scanD
+def generateRules(L, supportData, minConf=0.5):  #supportData is a dict coming from scanD
     bigRuleList = []
     for i in range(1, len(L)):#only get the sets with two or more items
         for freqSet in L[i]:
@@ -82,7 +82,7 @@ def calcConf(freqSet, H, supportData, brl, minConf=0.7):
             prunedH.append(conseq)
     return prunedH
 
-def rulesFromConseq(freqSet, H, supportData, brl, minConf=0.7):
+def rulesFromConseq(freqSet, H, supportData, brl, minConf=0.5):
     m = len(H[0])
     if (len(freqSet) > (m + 1)): #try further merging
         Hmp1 = aprioriGen(H, m+1)#create Hm+1 new candidates
@@ -100,54 +100,61 @@ def pntRules(ruleList, itemMeaning):
         print "confidence: %f" % ruleTup[2]
         print       #print a blank line
         
-            
-from time import sleep
-from votesmart import votesmart
-votesmart.apikey = 'a7fa40adec6f4a77178799fae4441030'
-#votesmart.apikey = 'get your api key first'
-def getActionIds():
-    actionIdList = []; billTitleList = []
-    fr = open('recent20bills.txt') 
-    for line in fr.readlines():
-        billNum = int(line.split('\t')[0])
-        try:
-            billDetail = votesmart.votes.getBill(billNum) #api call
-            for action in billDetail.actions:
-                if action.level == 'House' and \
-                (action.stage == 'Passage' or action.stage == 'Amendment Vote'):
-                    actionId = int(action.actionId)
-                    print 'bill: %d has actionId: %d' % (billNum, actionId)
-                    actionIdList.append(actionId)
-                    billTitleList.append(line.strip().split('\t')[1])
-        except:
-            print "problem getting bill %d" % billNum
-        sleep(1)                                      #delay to be polite
-    return actionIdList, billTitleList
-        
-def getTransList(actionIdList, billTitleList): #this will return a list of lists containing ints
-    itemMeaning = ['Republican', 'Democratic']#list of what each item stands for
-    for billTitle in billTitleList:#fill up itemMeaning list
-        itemMeaning.append('%s -- Nay' % billTitle)
-        itemMeaning.append('%s -- Yea' % billTitle)
-    transDict = {}#list of items in each transaction (politician) 
-    voteCount = 2
-    for actionId in actionIdList:
-        sleep(3)
-        print 'getting votes for actionId: %d' % actionId
-        try:
-            voteList = votesmart.votes.getBillActionVotes(actionId)
-            for vote in voteList:
-                if not transDict.has_key(vote.candidateName): 
-                    transDict[vote.candidateName] = []
-                    if vote.officeParties == 'Democratic':
-                        transDict[vote.candidateName].append(1)
-                    elif vote.officeParties == 'Republican':
-                        transDict[vote.candidateName].append(0)
-                if vote.action == 'Nay':
-                    transDict[vote.candidateName].append(voteCount)
-                elif vote.action == 'Yea':
-                    transDict[vote.candidateName].append(voteCount + 1)
-        except: 
-            print "problem getting actionId: %d" % actionId
-        voteCount += 2
-    return transDict, itemMeaning
+#
+# from time import sleep
+# from votesmart import votesmart
+# votesmart.apikey = 'a7fa40adec6f4a77178799fae4441030'
+# #votesmart.apikey = 'get your api key first'
+# def getActionIds():
+#     actionIdList = []; billTitleList = []
+#     fr = open('recent20bills.txt')
+#     for line in fr.readlines():
+#         billNum = int(line.split('\t')[0])
+#         try:
+#             billDetail = votesmart.votes.getBill(billNum) #api call
+#             for action in billDetail.actions:
+#                 if action.level == 'House' and \
+#                 (action.stage == 'Passage' or action.stage == 'Amendment Vote'):
+#                     actionId = int(action.actionId)
+#                     print 'bill: %d has actionId: %d' % (billNum, actionId)
+#                     actionIdList.append(actionId)
+#                     billTitleList.append(line.strip().split('\t')[1])
+#         except:
+#             print "problem getting bill %d" % billNum
+#         sleep(1)                                      #delay to be polite
+#     return actionIdList, billTitleList
+#
+# def getTransList(actionIdList, billTitleList): #this will return a list of lists containing ints
+#     itemMeaning = ['Republican', 'Democratic']#list of what each item stands for
+#     for billTitle in billTitleList:#fill up itemMeaning list
+#         itemMeaning.append('%s -- Nay' % billTitle)
+#         itemMeaning.append('%s -- Yea' % billTitle)
+#     transDict = {}#list of items in each transaction (politician)
+#     voteCount = 2
+#     for actionId in actionIdList:
+#         sleep(3)
+#         print 'getting votes for actionId: %d' % actionId
+#         try:
+#             voteList = votesmart.votes.getBillActionVotes(actionId)
+#             for vote in voteList:
+#                 if not transDict.has_key(vote.candidateName):
+#                     transDict[vote.candidateName] = []
+#                     if vote.officeParties == 'Democratic':
+#                         transDict[vote.candidateName].append(1)
+#                     elif vote.officeParties == 'Republican':
+#                         transDict[vote.candidateName].append(0)
+#                 if vote.action == 'Nay':
+#                     transDict[vote.candidateName].append(voteCount)
+#                 elif vote.action == 'Yea':
+#                     transDict[vote.candidateName].append(voteCount + 1)
+#         except:
+#             print "problem getting actionId: %d" % actionId
+#         voteCount += 2
+#     return transDict, itemMeaning
+
+
+if __name__ == '__main__':
+    data = loadDataSet()
+    L, supportData = apriori(data, minSupport = 0.5)
+    a = generateRules(L, supportData, minConf=0.7)
+    b = 1
